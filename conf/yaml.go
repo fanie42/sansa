9bc3e133f7058a6667c3954a7c38fdc1361165bb
[Stephanus Schoeman] <[sschoeman@sansa.org.za]>
2020-12-11 14:53:29 +0300

    changed parse input from filename to file

--- conf/yaml.go
+++ conf/yaml.go
@@ -1,14 +1,19 @@
 package conf
 
 import (
+    "fmt"
+    "io"
     "io/ioutil"
 
     "gopkg.in/yaml.v2"
 )
 
 // ParseYAML TODO
-func ParseYAML(filename string, config interface{}) error {
-    data := ioutil.ReadFile(filename)
+func ParseYAML(file io.Reader, config interface{}) error {
+    data, err := ioutil.ReadAll(file)
+    if err != nil {
+        return fmt.Errorf("could not read config file")
+    }
 
     return yaml.Unmarshal(data, config)
 }
