036eeece02b1dabb0d4e0cca4bce1aed8e93f52c
[Stephanus Schoeman] <[sschoeman@sansa.org.za]>
2020-12-11 14:31:18 +0300

    fixed yaml parser

--- conf/yaml.go
+++ conf/yaml.go
@@ -1,10 +1,14 @@
 package conf
 
 import (
+    "io/ioutil"
+
     "gopkg.in/yaml.v2"
 )
 
 // ParseYAML TODO
 func ParseYAML(filename string, config interface{}) error {
-    return yaml.Unmarshal(filename, config)
+    data := ioutil.ReadFile(filename)
+
+    return yaml.Unmarshal(data, config)
 }
